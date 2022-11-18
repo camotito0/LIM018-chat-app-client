caa7392ed92772b572a03b290726788f19c920af
camotito0 <cecilianallerena@gmail.com>
2022-11-18 13:01:13 -0500

    adding html layout to register component

--- src/components/registerView.jsx
+++ src/components/registerView.jsx
@@ -1,9 +1,41 @@
-/* eslint-disable */
+import {IoIosArrowBack} from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 function Register() {
-    return (
-        <main></main>
-    )
+  return (
+    <main className='register-main'>
+      <Link to='/'><button><IoIosArrowBack/></button></Link>
+      <h2>Welcome! Let's get<br/>started</h2>
+      <form>
+        <input
+          type='text'
+          name='username'
+          placeholder='Enter your username'
+          onChange={}
+          required
+        />
+        <input
+          type='email'
+          name='email'
+          placeholder='Enter your email'
+          onChange={}
+          required
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Create a password'
+          onChange={}
+          required
+        />
+        <button>Agree and Register</button>
+      </form>
+      <div>
+        <p>Do you have an account?</p>
+        <Link to='/login'>Login</Link>
+      </div>
+    </main>
+  )
 }
 
 export default Register;
